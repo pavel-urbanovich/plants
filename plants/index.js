539700e24c7e42d4ba179533d77f638336bea0b8
Pavel Urbanovich <yrbik@mail.ru>
2022-11-09 23:04:32 +0300

    feat: add index.js, script and styles for burger

--- plants/index.js
+++ plants/index.js
@@ -1,8 +1,25 @@
 // burger-menu
 const burger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+const body = document.querySelector('.body');
+const container = document.querySelector('.header-container');
+
 
 function openMenu() {
-  burger.classList.toggle('open')
+    burger.classList.toggle('open');
+    navigation.classList.toggle('active');
+    body.classList.toggle('active');
+  
 }
 
 burger.addEventListener('click', openMenu);
+
+function closeMenu (event) {
+  if (event.target.classList.contains('nav-link')) {
+    navigation.classList.remove('active');
+    body.classList.remove('active');
+    burger.classList.remove('open');
+  }
+}
+
+navigation.addEventListener('click', closeMenu);
