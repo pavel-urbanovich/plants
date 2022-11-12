64ced5222ca3d42ff302a67dacfbe3381f70986e
Pavel Urbanovich <yrbik@mail.ru>
2022-11-12 21:58:43 +0300

    feat: add function addBlurEffect

--- plants/index.js
+++ plants/index.js
@@ -1,10 +1,15 @@
-// burger-menu
 const burger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 const body = document.querySelector('.body');
 const container = document.querySelector('.header-container');
+const images = [...document.querySelectorAll('.image-content')];
+const controlWrapper = document.querySelector('.control-items');
+const controlButtons = [...document.querySelectorAll('.control-button')].splice(0, 3);
+let switchButton;
 
 
+
+// burger-menu
 function openMenu() {
     burger.classList.toggle('open');
     navigation.classList.toggle('active');
@@ -32,3 +37,65 @@ function closeMenu (event) {
 }
 
 navigation.addEventListener('click', closeMenu);
+
+//blur effect in section service
+
+// function addBlurEffect(event) {
+//   let buttonName = event.target.innerHTML.slice(0, 3).toLowerCase();
+
+//   images.forEach(item => {
+//     if (buttonName !== item.dataset.name && !item.classList.contains('effect')) {
+//       if (!effect) {
+//         item.classList.add('effect');
+//         event.target.classList.add('active-button');
+        
+//       }
+//     }
+//     if (buttonName === item.dataset.name && item.classList.contains('effect')) {
+//       item.classList.remove('effect');
+//     }       
+//   })
+  
+// }
+
+
+function addBlurEffect(event) {
+  let buttonName = event.target.innerHTML.slice(0, 3).toLowerCase();
+  if (!event.target.classList.contains('active-button') && switchButton) {
+    console.log ('on');
+    images.forEach(item => {
+      if (buttonName === item.dataset.name && item.classList.contains('effect')) {
+        item.classList.remove('effect');
+        event.target.classList.add('active-button');
+        switchButton = true;
+        controlButtons.forEach(item => {
+          if (!item.classList.contains('active-button')) {
+          item.classList.add('pointer');
+          }
+        })
+      } 
+    })
+    return;
+  }
+  if (event.target.classList.contains('active-button') && switchButton === true) {
+    const blurImages = [...document.querySelectorAll('.effect')];
+    controlButtons.forEach(item => item.classList.remove('active-button'));
+    blurImages.forEach(item => item.classList.remove('effect'));
+    controlButtons.forEach(item => {
+      if (item.classList.contains('pointer')) {
+        item.classList.remove('pointer');
+      }
+    })
+    switchButton = false;
+  } else {
+    images.forEach(item => {
+      if (buttonName !== item.dataset.name && !item.classList.contains('effect')) {
+        item.classList.add('effect');
+        event.target.classList.add('active-button');
+        switchButton = true;
+      }      
+    })
+  }
+}
+
+controlWrapper.addEventListener('click', addBlurEffect)
