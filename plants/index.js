79c43ee1de5e14fc49b0ce147049364a34291a99
Pavel Urbanovich <yrbik@mail.ru>
2022-11-18 23:56:38 +0300

    feat: add button, styles in contacts

--- plants/index.js
+++ plants/index.js
@@ -6,6 +6,30 @@ const images = [...document.querySelectorAll('.image-content')];
 const controlWrapper = document.querySelector('.control-items');
 const controlButtons = [...document.querySelectorAll('.control-button')].splice(0, 3);
 const priceContainer = document.querySelector('.price-btns');
+const select = document.querySelector('.select');
+const selectButton = document.querySelector('.contact-select');
+const contactsArray = [
+  {
+    'City': 'Canandaigua, NY',
+    'Phone': '+1	585	393 0001',
+    'Office adress:': '151 Charlotte Street',
+  },
+  {
+    'City': 'New York City',
+    'Phone': '+1	212	456 0002',
+    'Office adress:': '9 East 91st Street',
+  },
+  {
+    'City': 'Yonkers, NY',
+    'Phone': '+1	914	678 0003',
+    'Office adress:': '511 Warburton Ave',
+  },
+  {
+    'City': 'Sherrill, NY',
+    'Phone': '+1	315	908 0004',
+    'Office adress:': '14 WEST Noyes BLVD',
+  },
+]
 let switchButton;
 
 
@@ -122,6 +146,59 @@ function showAccordionBody(event) {
   }
 }
 
-priceContainer.addEventListener('click', showAccordionBody)
+priceContainer.addEventListener('click', showAccordionBody);
+
+// select in section contacts
+
+function showSelect(event) {
+  if (event.target.classList.contains('select-arrow')){
+    event.target.nextElementSibling.nextElementSibling.classList.toggle('active');
+    event.target.classList.toggle('arrow-up');
+    event.target.nextElementSibling.classList.add('select-active');
+    const selectItems = [...document.querySelectorAll('.select-item')];
+    selectItems.forEach(item => item.addEventListener('click', closeSelect));
+    let selectchildrens = [...select.children];
+    if(selectchildrens.length > 3) {
+      select.lastChild.remove();
+    }
+  }
+}
+
+function closeSelect(e) {
+  const selectBody = document.querySelector('.select-body');
+  selectBody.classList.toggle('active');
+  selectButton.innerText = `${e.target.innerText}`;
+  addContactsBlock(selectButton.innerText);
+}
+
+select.addEventListener('click', showSelect);
+
+function addContactsBlock(name) { 
+  const contactObject = document.createElement('div');
+  contactObject.classList.add('contact-object');
+  select.append(contactObject);
+  const contactWrap = document.createElement('div');
+  contactWrap.classList.add('contact-wrap');
+  contactObject.append(contactWrap);
+  let resultObj = Object.entries(contactsArray.find(item => item['City'] === `${name}`));
+  resultObj.forEach(item => {
+    const name = document.createElement('div');
+    name.classList.add('contact-title');
+    name.innerHTML = `${item[0]}`;
+    contactWrap.append(name);
+    const content = document.createElement('div');
+    content.classList.add('contact-content');
+    content.innerHTML = `${item[1]}`;
+    contactWrap.append(content);
+  });
+  const telButton = document.createElement('a');
+  telButton.classList.add('tel-button');
+  telButton.innerHTML = `Call us`;
+  const telContent = [...document.querySelectorAll('.contact-content')].find(item => item.innerHTML.startsWith('+'));
+  telButton.href = `tel:${telContent.innerHTML}`;
+  contactObject.append(telButton);
+}
+
+
 
 
